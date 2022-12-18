88a699c8da5b89769067ab89b777a74ca858a1ab
OliverC4 <oliver.canosa@yahoo.com>
2022-12-17 21:36:25 -0300

    use writeColor function for PPM image

--- engine.cpp
+++ engine.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include "vector.h"
+#include "color.h"
 
 int main() {
-
     // Image
     const int image_width = 256;
     const int image_height = 256;
@@ -12,15 +13,12 @@ int main() {
     for (int j = image_height - 1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << "\n" << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0.25;
-
-            int ir = static_cast<int>(256 * r);
-            int ig = static_cast<int>(256 * g);
-            int ib = static_cast<int>(256 * b);
-
-            std::cout << ir << " " << ig << " " << ib << "\n";
+            color pixelColor(double(i) / (image_width - 1),
+                             double(j) / (image_height - 1),
+                             0.25);
+            writeColor(std::cout, pixelColor);
         }
     }
+
+    std::cerr << "\nDone. \n";
 }
